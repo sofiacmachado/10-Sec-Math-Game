cf322f015455cef1d9319edffa0b07e77d2bc295
sofiacmachado <85806152+sofiacmachado@users.noreply.github.com>
2021-09-28 15:58:37 +0100

    Higher Score Defined

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-    let slider = document.getElementById('number-limit');
-    let sliderDisplay = document.getElementById('number-output');
+    const slider = document.getElementById('number-limit');
+    const sliderDisplay = document.getElementById('number-output');
     sliderDisplay.innerHTML = slider.value;    
+    const addition = document.getElementById('#equation-plus');
+    const subtraction = document.getElementById('#equation-minus');
+    const multiplication = document.getElementById('#equation-times');
+    const division = document.getElementById('#equation-divide');
     let currentQuestion;
     let timeLeft = 10;
     let interval;
     let score = 0;
- 
+    let higherScore = 0;
+    let operator = '+';
 
     const updateTimeLeft = function (amount) {
         timeLeft += amount;
@@ -25,38 +30,77 @@ $(document).ready(function() {
         $('#score').text(score);
     }
 
+    /* Update Higher Score */
+
+    const updateHigherScore = function () {
+        if (score > higherScore) {
+            higherScore = score;
+        }
+        $('#higher-score').text(higherScore);
+    }
+
+
     /* Set Timer to Start Game */
     const startGame = function () {
         if(!interval) {
             if (timeLeft === 0) {
                 updateTimeLeft(10);
                 updateScore(-score);
+                
             }
             interval = setInterval(function () {
                 updateTimeLeft(-1);
                 if(timeLeft === 0) {
                     clearInterval(interval);
                     interval = undefined;
+                    updateHigherScore();
                 }
             }, 1000);
         }
     }
 
- 
-
     const randomNumberGenerator = function(size) {
-            return Math.ceil(Math.random() * size);
+        return Math.ceil(Math.random() * size);
+    }
+
+    const getOperator = function() {
+        if (addition.checked) {
+            return operator = '+';
+        } else if (subtraction.checked) {
+            return operator = '-';
+        } else if (multiplication.checked) {
+            return operator = '*';
+        } else if (division.checked) {
+            return operator = '/';
         }
+    }
+
+  /*   $(document).on('click', '.equation-type', function () {
+        if ('#equation-plus:focus') {
+            operator = '+';
+        } else if ($('#equation-minus:focus')) {
+            operator = '-';
+        } else if ($('#equation-times:focus')) {
+            operator = '*';
+        } else if ($('#equation-divide:focus')) {
+            operator = '/';
+        }
+
+        return operator;
+    }) */
 
     const questionGenerator = function () {
         let question = {};
         let num1 = randomNumberGenerator(slider.value);
         let num2 = randomNumberGenerator(slider.value);
         
+
+
         question.answer = num1 + num2;
-        question.equation = String(num1) + ' + ' + String(num2);
+        question.equation = String(num1) + operator + String(num2);
 
         return question;
+        
     }
 
     const renderNewQuestion = function() { 
@@ -75,8 +119,6 @@ $(document).ready(function() {
             updateScore(+1);
         }
     }
-
-
     
     $('#user-input').on('keyup', function() {
         startGame();
@@ -84,6 +126,6 @@ $(document).ready(function() {
     });
 
     renderNewQuestion();
-
-    console.log('number-limit')
+    console.log(operator);
+    console.log('number-limit');
 });
